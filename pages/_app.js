968e6e9836200af19e02e661646b939b448d185a
jnodes <116927121+jnodes@users.noreply.github.com>
2025-07-09 11:33:37 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <script src="https://cdn.tailwindcss.com"></script>
+        <title>Billetera IO - Biometric DeFi Wallet</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
